bdaccdcf593dbe6f090da55a018b5b9f5b943f5a
Nathan Delavictoire <nathan.delavictoire@gmail.com>
2018-02-05 18:39:46 +0100

    Tutorial ongoing. Adding route functionality along with the dashboard view

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [
+    //makes route directives available for use in the app module.
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];// expose the mock heroes  to the class
+
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {
+    //simple initialisation
+    // constructor shouldn't do anything
+   }
+
+  getHeroes(): void {
+     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+   }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
